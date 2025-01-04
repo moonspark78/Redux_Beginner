8218e40a76b29f0206bd6d6b4deaac8f04b7d7a7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-04 15:52:38 +0100

    redux init with react router

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/cart' element={<Cart/>}></Route>
+          </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
