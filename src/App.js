ab47010ecf1b48cf1c855e9a6cb42e6396629138
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-04 18:45:07 +0100

    redux: working on the navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
+        <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/cart' element={<Cart/>}></Route>
