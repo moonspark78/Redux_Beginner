f8c656674e63c04a77b024917d2b799e08099f9f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-06 18:23:39 +0100

    Redux:  call API in Products section

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Products = () => {
+     const [products, setProducts] = useState([])
+
+
+     useEffect(() =>{
+        const fetchProducts = async () => {
+            const response = await fetch('https://fakestoreapi.com/products')
+            const data = await response.json()
+            setProducts(data)
+            console.log(data);
+        }
+        fetchProducts();
+     },[])
+
+
   return (
     <div>Products</div>
   )
