61090ab2eda3c1a90ace52770f74b7937d221bd2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-10 19:37:38 +0100

    Store:  create the store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div style={{display: 'flex', alignItems: 'space-between'}}>
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
         <span>REDUX STORE</span>
         <div>
             <Link className='navLink' to="/">Home</Link>
