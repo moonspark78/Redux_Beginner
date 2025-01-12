40198b5f24c3741adb314b6f60f7411484f5a12a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-12 20:54:21 +0100

    Cart: working on cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { remove } from '../store/cartSlice';
 
 const Cart = () => {
-
+  const dispatch = useDispatch();
   const products = useSelector((state) => state.cart)
+  const handleRemoveProduct = (productId) =>{
+    dispatch(remove(productId))
+  }
 
 
 
@@ -16,7 +20,7 @@ const Cart = () => {
               <img src={product.image} alt={product.title}/>
               <h5>{product.title}</h5>
               <h5>{product.price}</h5>
-              <button className='btn'>Remove</button>
+              <button onClick={() => handleRemoveProduct(product.id)} className='btn'>Remove</button>
             </div>
           ))}
         </div>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -15,10 +15,11 @@ const cartSlice = createSlice({
             state.push(action.payload)
         },
         remove(state, action) {
-            state = state.filter((item) => item.id !== action.payload);
+            return state.filter((item) => item.id !== action.payload);
         },
     }
 })
 
 export const {add, remove} = cartSlice.actions;
 export default cartSlice.reducer;
+
