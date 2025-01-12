7a3682afbe9be110a9d1751f51632716c1347338
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-01-12 06:30:08 +0100

    Cart: working on cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Cart = () => {
+
+  const products = useSelector((state) => state.cart)
+
+
+
   return (
-    <div>Cart</div>
+    <div>
+        <h3>Cart</h3>
+        <div className='cartWrapper'>
+          {products.mapp(product =>  (
+            <div></div>
+          ))}
+        </div>
+    </div>
   )
 }
 
